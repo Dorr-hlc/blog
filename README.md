cae67ef293a5f1fbde8b6844107bb40305cdb304
Dorr-hlc <1726660621@qq.com>
2022-04-14 09:23:09 +0800

    blog

--- README.md
+++
@@ -1,2 +0,0 @@
-# blog
-vuepress for blog
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'hlc-blog',
+    description: '~从前端到全栈开发~和码路工人一起学~',
+    base: '/blog/', 
+    themeConfig: {
+        nav: require('./nav'),
+        sidebar: require('./sidebar'),
+
+    },
+
+}
+
